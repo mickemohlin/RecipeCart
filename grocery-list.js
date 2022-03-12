5346619ab07ba4867a9d298856683c860ffaedb1
mickemohlin <mickemohlin@live.se>
2022-03-12 11:10:43 +0100

    Added content routing

--- grocery-list.js
+++ grocery-list.js
@@ -1,11 +1,20 @@
-
-
 class GroceryList {
     constructor() {
         this.date = Date.now();
+        this.groceries = [];
+        this.isDone = false;
     }
 
     getCreationDate() {
+        // TODO: format date before returning
         return this.date;
+    }
+
+    getGroceries() {
+        return this.groceries;
+    }
+
+    addGrocery(grocery) {
+        this.groceries.push(grocery);
     }
 }
--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+function homeButtonClicked() {
+    displaySection("welcome-section");
+}
+
 function recipesButtonClicked() {
-    console.log("Recipes button clicked!");
+    displaySection("recipe-section");
 }
 
 function shoppingListButtonClicked() {
-    console.log("Shopping List button clicked!")
+    displaySection("grocery-section");
+}
+
+function displaySection(section) {
+    switch(section) {
+        case "recipe-section":
+            document.getElementById("welcome-section").style.display = 'none';
+            document.getElementById("recipe-section").style.display = 'block';
+            document.getElementById("grocery-section").style.display = 'none';
+            break;
+        case "grocery-section":
+            document.getElementById("welcome-section").style.display = 'none';
+            document.getElementById("recipe-section").style.display = 'none';
+            document.getElementById("grocery-section").style.display = 'block';
+            break;
+        default:
+            document.getElementById("welcome-section").style.display = 'block';
+            document.getElementById("recipe-section").style.display = 'none';
+            document.getElementById("grocery-section").style.display = 'none';
+    }
 }
